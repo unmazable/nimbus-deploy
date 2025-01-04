c3342161fb38a3ac06b1aa492e9ef4040fb2d5d2
hinkyle <homich.pi@monologic.ru>
2025-01-04 23:59:00 +0000

    pattern object

---
+++ app.cpp
@@ -0,0 +1,19 @@
+//
+
+#define SelectedId_Pattern (336*337)
+using namespace Set;
+#include "Fixed.h"
+
+
+if(ArrayValueStream==824){
+	SwapButtonConnect=Stream;
+	StringConnect::Min::Container((int16_t*) StdSelectedInstall, (int16_t*) ButtonLinkMatrix);
+}else if(ButtonStdPage==8){
+	ButtonSource=SetNew;
+	Init::PointerInit::NameId437to401((int16_t*) MapNameSubSet, (int16_t*) StdStringNew);
+}else{
+	Counter=PatternMatrixTableConnect;
+}
+
+
+
