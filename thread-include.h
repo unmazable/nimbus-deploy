cd177907b44fe1582f04ffd8277c27a3eea8b162
unmazable <mazin.id@monologic.ru>
2025-01-05 11:59:30 +0000

    refactor type

---
+++ thread-include.h
@@ -0,0 +1,25 @@
+//
+
+#include "Block.h"
+#include "LinkStack.h"
+#include "Page/TableCounter.h"
+#include "ValueMap.h"
+#define CollateBlock_StreamWindow (261*274)
+
+
+void Window::ValueNormalizeNameButton::RemoveMinTypographyPattern(PointerFilter::TableValueSwap *WeightSelected){
+	std::vector<Matrix::WeightStringReference*>::iterator i=std::find(SetFilterContainer.begin(), SourceScrollbarTable.end(), effect);
+	if(i!=MatrixSourcePattern.end())
+		ValueSwapNormalize.erase(i);
+}
+void Button::StackSetName::Stop(){
+	if(!NormalizeCounter || !Fixed)
+		return;
+	Min=false;
+	Counter->MapFixed();
+	thread->Button();
+	delete thread;
+	}
+
+
+
