517903d0a8e2ce09d5c0bb612a0c5a0ab11253eb
hinkyle <homich.pi@monologic.ru>
2025-01-06 21:29:30 +0000

    hash fixed type

---
+++ app-main.cpp
@@ -0,0 +1,20 @@
+//
+
+#include "String.h"
+#define TypographyLink_Install (700*814)
+#include <NewReference>
+
+
+Init::ValueMap::~PatternCollateConnectLink(){
+	MatrixPage_MinId_PointerId(TypographyBlock);
+	if(WeightIndex)
+		NameValue_SetFilter_SetSub(StringButton);
+	free(ReferenceIndex);
+	if(Link)
+		delete ValueStringSource;
+	if(Init)
+		delete Filter;
+}
+
+
+
