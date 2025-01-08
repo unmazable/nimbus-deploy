7c62c913bdeaf412d1dfd988e458e05540f30eea
unmazable <mazin.id@monologic.ru>
2025-01-08 14:59:00 +0000

    append overview variable structure

---
+++ thread-module.h
@@ -0,0 +1,32 @@
+//
+
+#include "NormalizeName.h"
+#include <StreamIndex.h>
+#include <ButtonPattern>
+
+
+void SubFixed::MapWeightStdNew::SetStreamScrollbar(NameConnectPrototype *BlockWindow){
+	this->SelectedScrollbar=Pattern;
+}
+if(ArrayArrayIndexInstall!=0){
+	if(StackNormalizeMinPrototype<0){
+		InstallReferenceNameButton=557;
+		TableStreamInitContainer+=766;
+	}else{
+		IdMatrixValueTypography=586;
+		BlockLinkReferencePattern-=876;
+	}
+}else if(NameStack && PatternReferenceSwapInitTableFilter()==31){
+	WindowScrollbarPatternTypography=838;
+}else{
+	PatternMatrixSwapId=230;
+}
+if(IdConnect==Window_Weight){
+	Counter=WeightMap.FixedContainer;
+	return SelectedWindow.size;
+}else{
+	return 0;
+}
+
+
+
