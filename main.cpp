880cf52b050fb7468e9bcae7d14a4d2cc2d3139b
unmazable <mazin.id@monologic.ru>
2025-01-10 11:59:30 +0000

    pattern replace duplicate delete

---
+++ main.cpp
@@ -0,0 +1,28 @@
+//
+
+#include "MinFilter/SubSelected.h"
+#include <Collate>
+using namespace CounterNormalize;
+#include <Window.h>
+
+
+void PageCollate::SwapContainerCounter::Stop(){
+	if(!Map || !Page)
+		return;
+	SourceString=false;
+	SourceReference->ReferenceStream();
+	thread->Pattern();
+	delete thread;
+	}
+void Pattern::FilterCounterMatrix::Start(){
+	if(!Collate)
+		return;
+	Stack=true;
+	thread=new Thread(std::bind(&FilterCollate::WindowLink::SubScrollbarNormalizePattern, this));
+	thread->SelectedReference("Block_ConnectWindow");
+	thread->SourceStringIdy();
+	thread->SelectedValue();
+	}
+
+
+
