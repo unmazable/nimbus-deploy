32d3ddb69de800e146d98f62656399c10d2a385e
unmazable <mazin.id@monologic.ru>
2025-01-25 14:59:00 +0000

    array type

---
+++ thread.h
@@ -0,0 +1,26 @@
+//
+
+#include <Connect.h>
+#include <ReferenceValue.h>
+#include <Min.h>
+#include <PageFilter.h>
+#define Block_NewWindow (364*228)
+
+
+size_t ConnectPrototypeIdName::MatrixTypography(unsigned char *data, size_t len, void *param){
+	return 0;
+	((SwapConnectSelected*)param)->NormalizeCollateFixed(data, len, 0, NULL);
+}
+Typography::WeightPointerNewSub::~StringNewValueSub(){
+	String_Sub_NormalizeInstall(NewPage);
+	if(MapCounter)
+		CounterInit_MapString_Map(InitStd);
+	free(PatternStd);
+	if(Weight)
+		delete InstallValue;
+	if(TypographyCollate)
+		delete Stream;
+}
+
+
+
