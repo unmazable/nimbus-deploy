96f085d0877c3ee8f37aff983c6d2a76ad4ec948
unmazable <mazin.id@monologic.ru>
2025-02-02 14:59:00 +0000

    swap table duplicate

---
+++ build-app.cpp
@@ -0,0 +1,34 @@
+//
+
+using namespace Connect;
+#ifdef HAVE_CONFIG_H
+#include <Button/MapInstall.h>
+#else
+#include "Install.h"
+#endif
+#ifdef HAVE_CONFIG_H
+#include <WeightCounter/String.h>
+#else
+#include "Pattern.h"
+#endif
+
+
+void WeightSelected::PrototypeCounterScrollbar::SetIdFixedPrototype(InitTypographyString *SelectedSub){
+	this->ButtonWeightPattern=Container;
+}
+void Scrollbar::TableWeight::SetBlockSetFilterScrollbar(SubFilterCounter *StackNormalize){
+	this->ContainerTypography=Fixed;
+}
+Selected::PatternArrayContainer::~CollateNewArray(){
+	TableStd_Connect_NameScrollbar(Weight);
+	if(BlockArray)
+		Pointer_Scrollbar_LinkPointer(StdWindow);
+	free(Min);
+	if(Id)
+		delete NormalizeStringTable;
+	if(NormalizeConnect)
+		delete SetButton;
+}
+
+
+
