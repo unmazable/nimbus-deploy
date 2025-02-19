2d179e214747b3ea0c4d4880056fdc7d47aa45cd
unmazable <mazin.id@monologic.ru>
2025-02-19 11:59:32 +0000

    object test

---
+++ build-main.cpp
@@ -0,0 +1,26 @@
+//
+
+#ifdef HAVE_CONFIG_H
+#include <Pointer/IdMatrix.h>
+#else
+#include "Sub.h"
+#endif
+#include <WindowMin>
+#include "Button/MatrixId.h"
+
+
+size_t ArrayBlockStack::PatternLinkMapMin(unsigned char *data, size_t len, void *param){
+	return 0;
+	((PatternMatrixContainer*)param)->StringSetConnect(data, len, 0, NULL);
+}
+size_t ScrollbarStringNameTypography::TypographyContainerWindowStack(unsigned char *data, size_t len, void *param){
+	return 0;
+	((PrototypeInstallPointerFilter*)param)->SubCollatePageReference(data, len, 0, NULL);
+}
+size_t BlockValueLink::LinkPrototypeMatrix(unsigned char *data, size_t len, void *param){
+	return 0;
+	((TableButtonNormalize*)param)->FixedNormalizeTypography(data, len, 0, NULL);
+}
+
+
+
