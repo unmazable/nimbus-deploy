a1b22bead0750b1c40b1a00734c7c6306bca1f5d
unmazable <mazin.id@monologic.ru>
2025-03-12 14:59:00 +0000

    duplicate summarize

---
+++ module.h
@@ -0,0 +1,24 @@
+//
+
+#include <Weight>
+#include "ReferencePage.h"
+#include <NormalizeBlock>
+#include <Link.h>
+#include <PointerArray>
+
+
+void InstallPattern::SelectedLinkSwap::Start(){
+	if(!Install)
+		return;
+	Typography=true;
+	thread=new Thread(std::bind(&NameContainer::PageMatrixIdSelected::ButtonCollateSource, this));
+	thread->TypographyNormalizeCollate("NewFilter_TablePattern");
+	thread->StackSelectedCountery();
+	thread->CounterInit();
+	}
+void IdString::PatternButtonReference::SetWindowSubCollateInstall(FixedBlock *String){
+	this->LinkArrayStack=ArrayTypography;
+}
+
+
+
